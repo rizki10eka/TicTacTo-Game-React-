5563a0ef58e34592c9179b675ecb1497d6c22fdc
= <=>
2024-08-16 15:05:50 +0700

    reducing state management & identifying unnecessary state

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,36 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
-function App() {
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
 
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "0";
+  }
+  return currentPlayer;
+}
+
+function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-
-      if(prevTurns.length >0 && prevTurns[0].player === 'X') {
-        currentPlayer = '0';
-      }
-
-      const updatedTurns = [{ square: { 
-                row: rowIndex, 
-                col: colIndex 
-              }, player:activePlayer }, 
-              ...prevTurns];
+      const updatedTurns = [
+        {
+          square: {
+            row: rowIndex,
+            col: colIndex,
+          },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
 
       return updatedTurns;
     });
@@ -36,24 +46,20 @@ function App() {
           <Player
             initialName="Player 1"
             symbol="X"
-            isActive={activePlayer === 'X'}
+            isActive={activePlayer === "X"}
           />
           <Player
             initialName="Player 2"
             symbol="O"
-            isActive={activePlayer === 'O'}
+            isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          turns ={gameTurns}
-        />
-
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
 
-      <Log />
+      <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
